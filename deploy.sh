04a786ffa6c59e0e55006fdaef5a4f2e0ee8626a
Jeremy Pruitt <jeremypruitt@me.com>
2017-02-04 02:34:57 +0000

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -b "https://trailswehike.github.io" --theme=hugo-creative-theme
+hugo -b "https://www.trailswehike.com" --theme=hugo-creative-theme
 
 # Go To Public folder
 cd public
